0953282e5087206c97a73bc71278c504b3cfa892
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2020-04-08 18:18:57 -0300

    including unit test

--- src/App/Simple.test.js
+++ src/App/Simple.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from '../App'
+import TestPass from './TestPass'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const { getByText } = render(<TestPass />)
+  const linkElement = getByText(/Simple Text/i)
   expect(linkElement).toBeInTheDocument()
 })
---
+++ src/App/TestPass.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class TestPass extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Simple Text</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default TestPass
